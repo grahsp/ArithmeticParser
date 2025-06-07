babe697843b983bf59b0e1f1760abd7263b1549f
grahsp <pontus.grahsp@gmail.com>
2025-06-07 14:01:09 +0200

    test: initialize test project and add tests for parser

---
+++ ArithmeticParser.Tests/ParserTests.cs
@@ -0,0 +1,50 @@
+﻿using Lexer;
+
+namespace ArithmeticParser.Tests;
+
+[TestClass]
+public sealed class ParserTests
+{
+    [DataTestMethod]
+    [DataRow(0)]
+    [DataRow(125)]
+    [DataRow(int.MaxValue)]
+    public void Parse_ParseNumber(int source)
+    {
+        // Arrange
+        var tokens = new Token<ArithmeticType>[]
+            { new(ArithmeticType.Number, source.ToString()) };
+        
+        var parser = new ArithmeticParser(tokens);
+
+        // Act
+        var expression = parser.Parse();
+        var result = expression.Evaluate();
+        
+        // Assert
+        Assert.AreEqual(source, result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
